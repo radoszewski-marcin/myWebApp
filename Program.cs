496996b101ddfc207fae9be91c7c9fd64972cf0f
radoszewski-marcin <nicrovishion@gmail.com>
2023-11-02 11:11:40 +0100

    Wrong implementation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-// new line
+public class Program
+{
+    // Editing the same line (file from feature-2 branch)
+    public static void Main(string[] args)
+    {
+        BuildWebHost(args).Run();
+    }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
 
 var builder = WebApplication.CreateBuilder(args);
 
